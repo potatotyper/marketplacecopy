cbedba7390280cf0331446e4480ca2b051176512
jjforce17 <79370579+jjforce17@users.noreply.github.com>
2024-07-12 21:50:38 +0700

    Created current user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,8 +3,5 @@ class MainController < ApplicationController
         if flash[:notice] == nil
             flash[:notice] = "Welcome to my page!"
         end
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
     end
 end
